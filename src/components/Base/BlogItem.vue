51906be7653070fdcda651ce52907b705ad44e2d
235 <krahs@gmai.com>
2024-08-17 22:19:35 +0500

    Сделал блок с комментариями к блогу

--- src/components/Base/BlogItem.vue
+++ src/components/Base/BlogItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="blog-item flex flex-col gap-2.5">
     <img :src="`src/assets/img/blog-items/${blog.img}.png`" alt="" class="cursor-pointer" @click="$emit('openBlog', blog)">
-    <BlogItemStat :created-at="blog.createdAt" :comments="blog.comments" />
+    <BlogItemStat :created-at="blog.createdAt" :comments="blog.comments.length" />
     <h3 class="title text-xl/5">{{ blog.title }}</h3>
     <h5 class="description text-base/4">{{ blog.description }}</h5>
     <UiListTags class="gap-2.5 mt-auto" :tags="blog.tags" :tag-class="'text-sm'"/>
--- src/components/Base/BlogItemModal.vue
+++ src/components/Base/BlogItemModal.vue
@@ -5,11 +5,18 @@
         <i class="icon icon-cross-x absolute top-4 right-4 text-xl cursor-pointer font-semibold" @click="$emit('close')"></i>
         <h3 class="title text-2xl/6 font-semibold">{{ blog.title }}</h3>
         <div class="content flex flex-col">
-          <BlogItemStat :created-at="blog.createdAt" :comments="blog.comments" />
+          <BlogItemStat :created-at="blog.createdAt" :comments="blog.comments.length" />
           <img :src="`src/assets/img/blog-items/${blog.img}.png`" alt="" @click="$emit('openBlog', blog)">
           <p class="text text-base/[25px] font-medium">{{ blog.text }}</p>
         </div>
-        <UiListTags class="gap-2.5 mt-auto" :tags="blog.tags" :tag-class="'text-sm'"/>
+        <UiListTags class="gap-2.5 mb-1.5" :tags="blog.tags" :tag-class="'text-sm'"/>
+        <div class="comments flex flex-col gap-2.5">  
+          <h4 class="comments-title text-base font-semibold">Комментариев {{ blog.comments.length }}</h4>
+          <CommentInput />
+          <div class="comments-list p-3 flex flex-col gap-3">
+            <CommentItem v-for="(comment, index) in blog.comments" :key="index" :comment="comment"/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,6 +26,8 @@
 import { ref } from 'vue';
 import UiListTags from '@/components/Base/UiListTags.vue';
 import BlogItemStat from '@/components/Base/BlogItemStat.vue';
+import CommentItem from '@/components/Base/CommentItem.vue';
+import CommentInput from '@/components/Base/CommentInput.vue';
 
 const props = defineProps({
   blog: { type: Object }
@@ -33,6 +42,14 @@ const props = defineProps({
   }
   .content {
     gap: 0.9375rem;
+  }
+  .wrapper {
+    scrollbar-width: none; /* Для Firefox */
+    -ms-overflow-style: none;
+    ::-webkit-scrollbar {
+      width: 0px;  /* Скрываем скроллбар в Webkit браузерах (Chrome, Safari) */
+      background: transparent;  /* Делаем фон скроллбара прозрачным */
+    }
   }
 }
 </style>
