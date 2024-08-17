a35c85ddc4a836ef5b5c5f7bd4d55e0b2d754e2d
235 <krahs@gmai.com>
2024-08-17 15:11:36 +0500

    Изменил input поиска

--- src/components/Base/Input/UiSearch.vue
+++ src/components/Base/Input/UiSearch.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="search px-2.5 flex gap-2.5 w-full items-center rounded-md h-10">
     <i class="icon icon-search text-sm"></i>
-    <input class="w-full bg-transparent" type="text" v-model="value" placeholder="Поиск">
-    <i class="icon icon-cross-x text-sm cursor-pointer" v-show="value !== ''" @click="value = ''"></i>
+    <input 
+      class="w-full bg-transparent" 
+      type="text" 
+      :value="modelValue" 
+      @input="updateValue($event.target.value)"
+      placeholder="Поиск"
+    >
+    <i 
+      class="icon icon-cross-x text-sm cursor-pointer" 
+      v-show="modelValue !== ''" 
+      @click="clearValue"
+    ></i>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 
-const value = ref('');
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  }
+});
+
+const emit = defineEmits(['update:modelValue', 'enter']);
+
+const updateValue = (value) => {
+  emit('update:modelValue', value);
+};
+
+const clearValue = () => {
+  emit('update:modelValue', '');
+};
 </script>
 
 <style lang="scss" scoped>
